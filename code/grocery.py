00fb64628d0af9bc54be645f362c934a1c676249
Emily Zhao <emilyzhao@nyu.edu>
2022-01-26 15:07:07 -0500

    assignment 1 fixes

---
+++ code/grocery.py
@@ -0,0 +1,10 @@
+# Item: Bread, Price: $ 2.99
+# Item: Eggs, Price: $ 1.99
+
+item_1 = "Cheese"
+item_2 = "Eggs"
+item_1_price = 3.99
+item_2_price = 1.99
+
+print("Item:", item_1, "Price: $", item_1_price)
+print("Item:", item_2, "Price: $", item_2_price)
---
+++ code/mad-lib.py
@@ -0,0 +1,7 @@
+noun = input ("Noun: ")
+verb = input ("Verb: ")
+adjective = input ("Adjective: ")
+adverb = input ("Adverb: ")
+
+# Below I will output the mad lib
+print ("The", adjective, noun, "was very hungry, so it decided to", adverb, verb, "to the nearest restaurant.")
